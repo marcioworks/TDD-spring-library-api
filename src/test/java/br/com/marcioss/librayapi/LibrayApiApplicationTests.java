e30e6da860604b88f2026689b57b725bbc3883dc
marcio <marcio.silva@somapay.com.br>
2022-02-21 12:55:49 -0300

    project created

---
+++ src/test/java/br/com/marcioss/librayapi/LibrayApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcioss.librayapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrayApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
